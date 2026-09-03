a0e758612abbc16580f9379a592192ecfc9e9c19
victoribanez30 <vyibanez@up.edu.ph>
2026-09-03 18:43:46 +0800

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({' "name"': 'TASK API', "version": "1.0", "endpoints": ["/tasks"]});
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
